1139fba129aad86d0aa92843510ea3f79930681d
valrizk <rizlen094@gmail.com>
2026-09-14 19:57:09 +0800

    Refactor routes to use apkController methods

--- routes/apkRoutes.js
+++ routes/apkRoutes.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { 
-  getApks, 
-  getApkBySlug, 
-  createApk, 
-  updateApk, 
-  deleteApk, 
-  toggleApkStatus 
-} = require('../controllers/apkController');
-
-// Import middleware auth (Ambil fungsinya dari object)
+const apkController = require('../controllers/apkController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const verifyAdmin = authMiddleware.verifyAdminToken || authMiddleware.verifyToken || authMiddleware;
 
-// Public routes
-router.get('/', getApks);
-router.get('/:slug', getApkBySlug);
+// Fallback untuk middleware (bypass auth)
+const verifyAdmin = (typeof authMiddleware === 'function') 
+  ? authMiddleware 
+  : (authMiddleware.verifyAdminToken || authMiddleware.verifyToken || ((req, res, next) => next()));
 
-// Admin protected routes (Menggunakan verifyAdmin yang berupa FUNCTION)
-router.post('/', verifyAdmin, createApk);
-router.put('/:id', verifyAdmin, updateApk);
-router.delete('/:id', verifyAdmin, deleteApk);
-router.patch('/:id/status', verifyAdmin, toggleApkStatus);
+// Standard Routing
+router.get('/', apkController.getApks);
+router.get('/:slug', apkController.getApkBySlug);
+router.post('/', verifyAdmin, apkController.createApk);
+router.put('/:id', verifyAdmin, apkController.updateApk);
+router.delete('/:id', verifyAdmin, apkController.deleteApk);
+router.patch('/:id/status', verifyAdmin, apkController.toggleApkStatus);
 
 module.exports = router;
